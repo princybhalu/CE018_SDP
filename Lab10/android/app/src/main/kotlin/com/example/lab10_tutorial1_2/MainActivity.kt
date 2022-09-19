123e0a07d1c9f332b8918cb103d2221e05e67a35
princybhalu <princybhalu11@gmail.com>
2022-09-19 22:05:10 +0530

    lab10

---
+++ Lab10/android/app/src/main/kotlin/com/example/lab10_tutorial1_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab10_tutorial1_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Lab10/lib/main.dart
@@ -0,0 +1,86 @@
+
+/*
+//Tutorial-1
+import 'package:flutter/material.dart';
+import 'choose_location.dart';
+import 'home.dart';
+import 'loading.dart';
+/*
+10
+void main() => runApp(MaterialApp(
+// home: Home(),
+routes: { // routes is Map variable with 'key:value' pairs
+// '/about' ....eg.....routes the screen through 'about' widgets
+// '/contact'....
+'/': (context) => Loading(), // base routes....base widget file...main
+file of project
+// above statement will creates error...because it conflict with 'home:
+Home(),
+// because both statements tell flutter to initialize the app from their
+given
+location
+'/home': (context) => Home(),
+'/location': (context) => ChooseLocation(),
+}
+));
+*/
+void main() => runApp(MaterialApp(
+// home: Home(),
+// instead of making home: property to make any page to initialize at beginning...
+// we can use following code ....
+// initialRoute: '/home',
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    }
+));
+*/
+
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'services/word_time.dart';
+void main() => runApp(MaterialApp(
+// home: Home(),
+// instead of making home: property to make any page to initialize at beginning...
+// we can use following code ....
+// initialRoute: '/home',
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Loading(),
+    }
+));
+class Loading extends StatefulWidget {
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+class _LoadingState extends State<Loading> {
+  String? time = 'LOADING..........';
+  void setWorldTime() async {
+    WordTime timeinstance =
+    WordTime(location: 'kolkata',flag: 'india.png',url: 'Asia/Kolkata');
+    await timeinstance.getTime();
+// print(timeinstance.time);
+    setState(() {
+      time = timeinstance.time;
+    });
+  }
+  @override
+  void initState() {
+    super.initState();
+    setWorldTime();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(60.0),
+          child: Text(time.toString()),
+        )
+    );
+  }
+}
+
